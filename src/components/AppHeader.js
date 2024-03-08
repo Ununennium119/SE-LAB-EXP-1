1141f0809637e29901073417e275a1bdd4c3e230
Mohammad Sadeghi <mohammad.s@smartech.ir>
2024-03-08 15:48:48 +0330

    feat: Add filter button

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Button from "./Button";
+import Button, { SelectButton } from "./Button";
 import styles from "../styles/modules/app.module.scss";
 
 function AppHeader() {
   return (
     <div className={styles.appHeader}>
       <Button variant="primary">Add Task</Button>
+      <SelectButton id="status">
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Completed</option>
+      </SelectButton>
     </div>
   );
 }
