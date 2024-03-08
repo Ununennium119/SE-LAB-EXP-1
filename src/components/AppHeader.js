f4cfef11eb00c92b142baf552c9fca6e432cd4b5
Mohammad Sadeghi <mohammad.s@smartech.ir>
2024-03-08 15:46:31 +0330

    feat: Add button component

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import Button from "./Button";
 import styles from "../styles/modules/app.module.scss";
 
 function AppHeader() {
   return (
     <div className={styles.appHeader}>
-      AppHeader
+      <Button variant="primary">Add Task</Button>
     </div>
   );
 }
