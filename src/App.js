8875d84934e186ff44f5a4c9040bef0b6d8e56b6
Mohammad Sadeghi <mohammad.s@smartech.ir>
2024-03-08 15:36:20 +0330

    feat: Add app header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PageTitle from "./components/PageTitle";
-import "./App.css";
+import AppHeader from "./components/AppHeader";
+import styles from "./styles/modules/app.module.scss";
 
 function App() {
   return (
     <>
       <div className="container">
         <PageTitle>TODO List</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+        </div>
       </div>
     </>
   );
---
+++ src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styles from "../styles/modules/app.module.scss";
+
+function AppHeader() {
+  return (
+    <div className={styles.appHeader}>
+      AppHeader
+    </div>
+  );
+}
+
+export default AppHeader;
